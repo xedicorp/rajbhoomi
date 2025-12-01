db2f466deca1063a34813e9440aeb113c72f4b4d
SibankarOfficial <159782127+SibankarOfficial@users.noreply.github.com>
2025-12-01 14:01:51 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rajbhoomi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
